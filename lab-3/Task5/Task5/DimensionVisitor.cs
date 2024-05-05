96c5f67671f0de36f6b48cf0cd06d998a6b813b3
olegveshchykov <ipz222_vom@student.ztu.edu.ua>
2024-05-06 00:45:57 +0300

    add feature Visitor

---
+++ lab-3/Task5/Task5/DimensionVisitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DimensionVisitor : IVisitor
+{
+    public void VisitElement(LightElementNode element)
+    {
+        // Обчислення розмірів елемента
+        Console.WriteLine($"Calculating dimensions for element: {element.TagName}");
+    }
+
+    public void VisitTextNode(LightTextNode textNode)
+    {
+        // Обчислення розмірів текстового вузла
+        Console.WriteLine($"Calculating dimensions for text node: {textNode.Text}");
+    }
+}
